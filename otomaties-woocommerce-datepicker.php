92f35fd54fe7a9a0b9d68f79ca7a725c9344bcbf
Tom Broucke <tom@tombroucke.be>
2023-06-22 10:36:03 +0200

    chore(bump): Bump version to 1.1.0

--- otomaties-woocommerce-datepicker.php
+++ otomaties-woocommerce-datepicker.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Otomaties WooCommerce Datepicker
  * Description:       Add a datepicker on the checkout page
- * Version:           1.0.1
+ * Version:           1.1.0
  * Author:            Tom Broucke
  * Author URI:        https://tombroucke.be/
  * License:           GPL-2.0+
