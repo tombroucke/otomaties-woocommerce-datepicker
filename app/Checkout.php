9822af50f57387f783c7f52e646ab7295eaee8c0
Tom Broucke <tom@tombroucke.be>
2024-10-29 14:53:57 +0100

    🩹 fix: add order meta data instead of update_post_meta to support hpos

--- app/Checkout.php
+++ app/Checkout.php
@@ -3,16 +3,17 @@
 namespace Otomaties\WooCommerce\Datepicker;
 
 use Illuminate\Support\Str;
-use Otomaties\WooCommerce\Datepicker\Facades\Options;
 use Otomaties\WooCommerce\Datepicker\Facades\Datepicker;
+use Otomaties\WooCommerce\Datepicker\Facades\Options;
 
 class Checkout
 {
-    public function validate($fields, $errors) {
-        if (!isset($_POST['otomaties-woocommerce-datepicker--date'])) {
+    public function validate($fields, $errors)
+    {
+        if (! isset($_POST['otomaties-woocommerce-datepicker--date'])) {
             return;
         }
-        
+
         $timeZone = new \DateTimeZone(wp_timezone_string());
         $dateTime = \DateTime::createFromFormat('Y-m-d', $_POST['otomaties-woocommerce-datepicker--date'], $timeZone);
         $invalidReason = Datepicker::isDateInvalid($dateTime);
@@ -22,40 +23,46 @@ class Checkout
         }
     }
 
-    public function saveDateToSession($data) {
+    public function saveDateToSession($data)
+    {
         parse_str($data, $postData);
         $date = $postData['otomaties-woocommerce-datepicker--date'] ?? null;
         $id = $postData['otomaties-woocommerce-datepicker--id'] ?? null;
         $this->saveDatepickerDataToSession($date, $id);
     }
 
-    public function updateSession() {
+    public function updateSession()
+    {
         $date = $_POST['otomaties-woocommerce-datepicker--date'] ?? null;
         $id = $_POST['otomaties-woocommerce-datepicker--id'] ?? null;
         $this->saveDatepickerDataToSession($date, $id);
     }
 
-    private function saveDatepickerDataToSession($date = null, $id = null) {
-        if (!$date || !$id) {
+    private function saveDatepickerDataToSession($date = null, $id = null)
+    {
+        if (! $date || ! $id) {
             return;
         }
-        
-        WC()->session->set('otomaties_woocommerce_datepicker_' . $id . '_date', wc_clean(wp_unslash($date)));
+
+        WC()->session->set('otomaties_woocommerce_datepicker_'.$id.'_date', wc_clean(wp_unslash($date)));
     }
 
-    public function saveDatepickerDate($orderId, $data, $order) {
+    public function saveDatepickerDate($orderId, $data, $order)
+    {
         $datepickerId = $_POST['otomaties-woocommerce-datepicker--id'] ?? null;
         $datepickerDate = $_POST['otomaties-woocommerce-datepicker--date'] ?? null;
 
         if ($datepickerId) {
             $datepicker = new \Otomaties\WooCommerce\Datepicker\Datepicker($datepickerId, Options::instance());
-            update_post_meta($orderId, 'otom_wc_datepicker_id', wc_clean(wp_unslash($datepickerId)));
-            update_post_meta($orderId, 'otom_wc_datepicker_label', $datepicker->administrationLabel());
+            $order->update_meta_data('otom_wc_datepicker_id', wc_clean(wp_unslash($datepickerId)));
+            $order->update_meta_data('otom_wc_datepicker_label', $datepicker->administrationLabel());
         }
 
         if ($datepickerDate) {
-            update_post_meta($orderId, 'otom_wc_datepicker_date', wc_clean(wp_unslash($datepickerDate)));
+            $order->update_meta_data('otom_wc_datepicker_date', wc_clean(wp_unslash($datepickerDate)));
         }
+
+        $order->save();
 
         $sessionData = collect(WC()->session->get_session_data());
         $sessionData
@@ -63,7 +70,7 @@ class Checkout
                 return Str::startsWith($key, 'otomaties_woocommerce_datepicker');
             })
             ->keys()
-            ->each(function ($key) use ($sessionData) {
+            ->each(function ($key) {
                 WC()->session->__unset($key);
             });
     }
